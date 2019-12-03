46bf65b8c8c6e44b5d37c13f37e0ba1280b5a7f1
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-12-03 11:32:55 -0500

    csv reader

---
+++ src/util/index.ts
@@ -0,0 +1,16 @@
+import { readFileSync } from "fs";
+import { parse } from "csv/lib/sync";
+
+export function getCsv(day: string) {
+  const input = (() => {
+    try {
+      return readFileSync(`src/data/${day}.csv`, `utf8`).replace(/\r/g, "");
+    } catch (error) {
+      console.error(error);
+    }
+  })();
+
+  const output = parse(input, {});
+
+  return output.flat();
+}
